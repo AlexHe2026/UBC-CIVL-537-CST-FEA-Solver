d1b7bd30e247a8e8d488f428b0fcac5a1518e5b5
Alex He <35545870+AlexHe2026@users.noreply.github.com>
2026-04-22 19:42:29 +0000

    Finalize Elements Code

--- src/elements.py
+++ src/elements.py
@@ -16,7 +16,13 @@ def compute_area(coords):
     float
         Signed area of the triangle (positive if nodes are counter-clockwise).
     """
-    raise NotImplementedError
+    # Get the coordinates
+    x0, y0 = coords[0]
+    x1, y1 = coords[1]
+    x2, y2 = coords[2]
+    
+    # Using Cross Product to get the signed area. 
+    return 0.5 * ((x1 - x0) * (y2 - y0) - (x2 - x0) * (y1 - y0))
 
 
 def compute_B(coords):
@@ -43,7 +49,26 @@ def compute_B(coords):
     The coefficients b_i and c_i come from cyclic permutations of the node coordinates.
     Refer to CIVL 537 Lecture Notes, Section 4.
     """
-    raise NotImplementedError
+    #Get Coordinates
+    x_i, y_i = coords[0]
+    x_j, y_j = coords[1]
+    x_k, y_k = coords[2]
+
+    #map the corrdinates with CIVL 537 Lecture Section 06 pg.20
+    b_i = y_j - y_k
+    b_j = y_k - y_i
+    b_k = y_i - y_j
+    c_i = x_k - x_j
+    c_j = x_i - x_k
+    c_k = x_j - x_i
+
+    #Calculate B Matrix to a 3x6 Matrix
+    B = 1 / (2*compute_area(coords))*np.array([
+        [b_i, 0, b_j, 0, b_k, 0],
+        [0, c_i, 0, c_j, 0, c_k],
+        [c_i, b_i, c_j, b_j, c_k, b_k]])
+    
+    return B
 
 
 def compute_D(E, nu, mode="plane_stress"):
@@ -70,7 +95,14 @@ def compute_D(E, nu, mode="plane_stress"):
     Plane strain: eps_zz = 0, eliminate sigma_zz from 3D Hooke's law.
     The resulting D matrices are different. Implement them based on the course notes.
     """
-    raise NotImplementedError
+
+    #Calculate Plane Stress per CIVL 537 Section 06 Pg. 14
+    D = (E/(1-nu**2))*np.array([
+        [1, nu, 0],
+        [nu, 1, 0],
+        [0, 0, (1-nu)/2]])
+    
+    return D
 
 
 def compute_k(coords, D, thickness):
@@ -96,4 +128,10 @@ def compute_k(coords, D, thickness):
     Since B is constant over the element, the integral simplifies to
     a single multiplication (no numerical quadrature needed).
     """
-    raise NotImplementedError
+
+    #K matrix for a CST is B transpose * D * B * thickness * area (CIVL 537 Section 6 Pg.20)
+    area = compute_area(coords)
+    B = compute_B(coords)
+    k = thickness * area * (B.T @ D @ B)
+    
+    return k
